9132826957052358fef5360cbc98c49ccee1a367
Alan Christy <alanchristy.thomas@ad.infosys.com>
2022-10-21 14:55:19 +0530

    Minor Updates

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import json
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -10,9 +10,16 @@ def handle_get_customers():
 
 
 @app.route('/api/customers/<int:customer_id>')
-def handle_get_customer_id():
-    return None, 200
+def handle_get_customer_by_id(customer_id):
+    # this handler method is not ready yet
+    return json.dumps({'id': customer_id}), 200
+
 
 @app.route('/')
 def index():
+    # the html files are located in "templates" folder
     return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=8080, host='0.0.0.0')
