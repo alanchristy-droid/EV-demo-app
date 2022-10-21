a0c7ea5383cac7816f2376d3b2041dad374c2ac1
Alan Christy <alanchristy.thomas@ad.infosys.com>
2022-10-21 14:48:39 +0530

    New Update

--- test.py
+++ test.py
@@ -1,43 +1,18 @@
-class stack():
-    def __init__(self):
-        self.new_stack=[]
-    
-    def add(self,value):
-        self.new_stack.append(value)
-    
-    def remove(self):
-        return self.new_stack.pop(-1)
-    
-    def __repr__(self) -> str:
-        return f'{self.new_stack}'
-stack1=stack()
-print(stack1)
-stack1.add(10)
-stack1.add(20)
-stack1.add(30)
-print(stack1)
-print(stack1.remove())
-print(stack1)
+import json
+from flask import Flask
+
+app = Flask(__name__)
 
 
+@app.route('/api/customers')
+def handle_get_customers():
+    return json.dumps([]), 200
 
-class Queue():
-    def __init__(self):
-        self.new_stack=[]
-    
-    def add(self,value):
-        self.new_stack.append(value)
-    
-    def remove(self):
-        return self.new_stack.pop(0)
-    
-    def __repr__(self) -> str:
-        return f'{self.new_stack}'
-stack1=Queue()
-print(stack1)
-stack1.add(10)
-stack1.add(20)
-stack1.add(30)
-print(stack1)
-print(stack1.remove())
-print(stack1)
+
+@app.route('/api/customers/<int:customer_id>')
+def handle_get_customer_id():
+    return None, 200
+
+@app.route('/')
+def index():
+    return render_template('index.html')
